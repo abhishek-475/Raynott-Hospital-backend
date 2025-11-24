8cfc325b1a896b06318f7b24c6e77611bb2d143d
Abhishek Pavithran <abhishekpavi6@gmail.com>
2025-11-24 11:27:58 +0530

    fifth

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { register, login, getMe, registerAdmin } = require('../controllers/authController');
-const { protect } = require('../middlewares/auth');
+const { protect, admin } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
-router.post('/register-admin', protect, admin, registerAdmin);
+router.post('/register-admin', protect, admin, registerAdmin); // Only admins can create other admins
 
 module.exports = router;
